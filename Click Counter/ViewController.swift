4a04996e36f58f901936d80a28061aa5c323b6b7
omrobbie <putu.roby@yahoo.com>
2018-03-31 06:21:03 +0800

    show button on the screen

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -19,6 +19,13 @@ class ViewController: UIViewController {
         label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
         label.text = "0"
         view.addSubview(label)
+        
+        // button
+        let button = UIButton()
+        button.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
+        button.setTitle("Click", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        view.addSubview(button)
     }
 
     override func didReceiveMemoryWarning() {
