c6288efedcfb103fbf36b563198c5255f27af6c1
omrobbie <putu.roby@yahoo.com>
2018-04-01 07:45:28 +0800

    setting target action

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var count = 0
+    var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +22,7 @@ class ViewController: UIViewController {
         label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
         label.text = "0"
         view.addSubview(label)
+        self.label = label
         
         // button
         let button = UIButton()
@@ -26,6 +30,13 @@ class ViewController: UIViewController {
         button.setTitle("Click", for: .normal)
         button.setTitleColor(UIColor.blue, for: .normal)
         view.addSubview(button)
+        
+        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControlEvents.touchUpInside)
+    }
+    
+    @objc func incrementCount() {
+        self.count += 1
+        self.label.text = "\(self.count)"
     }
 
     override func didReceiveMemoryWarning() {
